0c94654a533b9ff5b2fd7dd1642c16bf4f9c1932
VinceZCL <64774232+VinceZCL@users.noreply.github.com>
2020-05-04 23:37:11 +0800

    Update and rename webscrape.py to steam_game_links.py

--- steam_game_links.py
+++ steam_game_links.py
@@ -1,18 +1,26 @@
+# import BeautifulSoup from bs4 library
 from bs4 import BeautifulSoup
+# import url handling module
 import urllib.request, urllib.parse, urllib.error
 
+# Empty list to handle unfiltered links
 links = list()
-gamelinks = list()
   
+# URL for web scrape
+# Can be changed
 myurl = "https://store.steampowered.com/search/?sort_by=_ASC&ignore_preferences=1&specials=1&filter=topsellers"
 
+# Putting every element of the page into variable html
 html = urllib.request.urlopen(myurl).read()
+# Parsing the html into variable soup
 soup = BeautifulSoup(html, "html.parser")
 
+# Search for every elemt under <a> tag
 tags = soup("a")
 for tag in tags:
-    links.append(tag.get("href", None))
+    links.append(tag.get("href", None)) # Obtaining the links and putting into list links
 
+# Creating function to filter links
 def steamlink(links):
     if "store" and "app" in links:
         return True
@@ -21,6 +29,9 @@ def steamlink(links):
     else:
         return False
 
+# Applying filter towards links
 filteredlinks = filter(steamlink, links)
+
+# Printing filteredlinks
 for a in filteredlinks:
     print(a)
