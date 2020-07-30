af6eca2a22a4043b1a5fb6f8683b0f6341361401
VinceZCL <64774232+VinceZCL@users.noreply.github.com>
2020-07-31 01:37:27 +0800

    Update steam_game_links.py

--- steam_game_links.py
+++ steam_game_links.py
@@ -1,26 +1,27 @@
-# import BeautifulSoup from bs4 library
+# Import modules
 from bs4 import BeautifulSoup
-# import url handling module
 import urllib.request, urllib.parse, urllib.error
 
-# Empty list to handle unfiltered links
+# Creating list templates
 links = list()
-  
+prices = list()
+new_links = list()
+names = list()
+
 # URL for web scrape
-# Can be changed
+# Changable
 myurl = "https://store.steampowered.com/search/?sort_by=_ASC&ignore_preferences=1&specials=1&filter=topsellers"
 
-# Putting every element of the page into variable html
+# Creating request and obtaining HTML
 html = urllib.request.urlopen(myurl).read()
-# Parsing the html into variable soup
 soup = BeautifulSoup(html, "html.parser")
 
-# Search for every elemt under <a> tag
+# Getting list of links on the URL
 tags = soup("a")
 for tag in tags:
-    links.append(tag.get("href", None)) # Obtaining the links and putting into list links
+    links.append(tag.get("href", None))
 
-# Creating function to filter links
+# Filter function
 def steamlink(links):
     if "store" and "app" in links:
         return True
@@ -29,9 +30,37 @@ def steamlink(links):
     else:
         return False
 
-# Applying filter towards links
+# Link filtering
 filteredlinks = filter(steamlink, links)
-
-# Printing filteredlinks
 for link in filteredlinks:
-    print(link)
+    new_links.append(link)
+
+# Obtaining prices
+for num in range(0,len(new_links)):
+    price = soup.select(f"#search_resultsRows > a:nth-child({num}) > div.responsive_search_name_combined > div.col.search_price_discount_combined.responsive_secondrow > div.col.search_price.discounted.responsive_secondrow")
+    if price == []:
+        continue
+    else:
+        price_str = price[0].text
+        price_str = price_str.strip()
+        prices.append(price_str)
+
+# Obtaining names
+for num in range(0, len(new_links)):
+    name = soup.select(f"#search_resultsRows > a:nth-child({num}) > div.responsive_search_name_combined > div.col.search_name.ellipsis")
+    if name == []:
+        continue
+    else:
+        name_str = name[0].text
+        name_str = name_str.strip()
+        names.append(name_str)
+
+# Outputting all information
+for num in range(0,len(prices)):
+    print("Name: ")
+    print(names[num])
+    print("Link: ")
+    print(new_links[num])
+    print("Prices: (Original Price - New Price)")
+    print(prices[num])
+    print("----------------------------------------------------------------------------------------------------------------")
